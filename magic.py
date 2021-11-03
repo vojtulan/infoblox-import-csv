31fd5ba1589d871b8e75dfe6677251a96ef3cf29
vojtulan <52625189+vojtulan@users.noreply.github.com>
2021-11-03 19:57:28 +0100

    czech remap

--- magic.py
+++ magic.py
@@ -6,6 +6,27 @@ print("Script has started ...")
 print("Validating and repairing csv files ...")
 regex = "([0-9a-fA-F]:?){12}"
 
+czechChars = ["á", "č", "ď", "é", "ě", "í", "ň", "ó", "ř", "š", "ť", "ú", "ů", "ý", "ž","Á", "Č", "Ď", "É", "Ě", "Í", "Ň", "Ó", "Ř", "Š", "Ť", "Ú", "Ů", "Ý", "Ž"]
+remap = ["a", "c", "d", "e", "e", "i", "n", "o", "r", "s", "t", "u", "u", "y", "z","A", "C", "D", "E", "E", "I", "N", "O", "R", "S", "T", "U", "U", "Y", "Z"]
+
+
+def Translate(string):
+    translatedString = ""
+    charList = [char for char in string]
+    for char in charList:
+        isCharRemaped = False
+        for czechCharIndex in range(0, len(czechChars) - 1):
+            if char == czechChars[czechCharIndex]:
+                translatedString += remap[czechCharIndex]
+                isCharRemaped = True
+                break
+
+        if not isCharRemaped:
+            translatedString += char
+
+    return translatedString
+
+
 headerHostAddress = "header-hostaddress;address*;_new_address;parent*;boot_file;boot_server;broadcast_address;configure_for_dhcp;configure_for_dns;deny_bootp;domain_name;domain_name_servers;ignore_dhcp_param_request_list;lease_time;mac_address;match_option;network_view;next_server;option_logic_filters;pxe_lease_time;pxe_lease_time_enabled;routers;use_for_ea_inheritance;view"
 headerHostRecord = "header-hostrecord;fqdn*;_new_fqdn;addresses;aliases;cli_credentials;comment;configure_for_dns;_new_configure_for_dns;created_timestamp;creator_member;ddns_protected;disabled;enable_discovery;enable_immediate_discovery;ipv6_addresses;network_view;override_cli_credentials;override_credential;snmpv1v2_credential;snmpv3_credential;ttl;use_snmpv3_credential;view"
 templateHostRecord = "hostrecord;P20325;;10.32.22.2;;;;FALSE;;;;FALSE;FALSE;TRUE;FALSE;;Default;;FALSE;;;;FALSE;"
@@ -102,10 +123,11 @@ for filename in os.listdir('csv'):
             clearSpaces = row[1].replace(" ","")
             hostRecord[1] = clearSpaces.replace("(","_")
             hostRecord[1] = hostRecord[1].replace(")","")
+            hostRecord[1] = Translate(hostRecord[1])
             #TADY UPRAVUJ A PRIDAVEJ jen zkopiruj a uncomment
             #priklad - nahradis ? za underscore atd
             
-            hostRecord[1] = hostRecord[1].replace("ž","TEST")
+            #hostRecord[1] = hostRecord[1].replace("/","TEST")
             
             #TADY UZ MI NA TO ZASE NESAHEJ
             hostAddress[3] = hostRecord[1]
@@ -120,6 +142,7 @@ for filename in os.listdir('csv'):
             r.write(i + "\n")
         for i in hostAddressArray:
             r.write(i + "\n")
+
 
 print("Script has ended, check the newly created 'infobloxImport.csv' ")
             
